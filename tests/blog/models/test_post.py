7f0fbe02be3210b4a9799d8126dfe35e471376c2
Jianying Sheng <j6sheng@uwaterloo.ca>
2020-10-17 22:05:19 -0400

    adding test model method

--- tests/blog/models/test_post.py
+++ tests/blog/models/test_post.py
@@ -22,3 +22,8 @@ def test_published_posts_only_returns_those_with_published_status():
     result = list(Post.objects.published())
 
     assert result == expected
+
+def test_publish_sets_status_to_published():
+    post = mommy.make('blog.Post', status=Post.DRAFT)
+    post.publish()
+    assert post.status == Post.PUBLISHED
